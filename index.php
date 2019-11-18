baff87261c43c79b21c7c39214d6ee0b7974ed1b
vdebes <vdebes@gmail.com>
2019-11-18 20:19:47 +0100

    add dummy class

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use vdebes\phpboilerplate\Foo;
+
+$foo = new Foo();
+$foo->bar();
+
+?>
---
+++ src/Foo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace vdebes\phpboilerplate;
+
+class Foo
+{
+    public function bar()
+    {
+        return true;
+    }
+}
